b2be1bb5054da7a42e918a3030522bfc236416d8
mayue <mayue641246183@163.com>
2018-08-07 11:40:40 +0800

    配置文件+页面配置+路由配置

---
+++ src/routes/router.js
@@ -0,0 +1,14 @@
+import VueRouter from 'vue-router';
+
+import Vue from '../../node_modules/vue';
+
+Vue.use(VueRouter);
+
+var router = new VueRouter({
+
+    routes: [
+
+    ]
+});
+
+export default router;
